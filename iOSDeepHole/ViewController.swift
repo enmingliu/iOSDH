6637b9fdc97e99b6b56776fde0b2c6c9bfb406b1
Bill Liu <bill.liu.school@gmail.com>
2019-03-30 17:07:34 -0700

    chagenddd

--- iOSDeepHole/ViewController.swift
+++ iOSDeepHole/ViewController.swift
@@ -148,10 +148,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             locationBuffer.remove(at: 0)
         }
         
-        let image = UIImage(data: imageData)
+        let image = imageWithImage(image: UIImage(data: imageData)!, scaledToSize: CGSize(width: 100, height: 100))
+        print(image.size)
         imageView.backgroundColor = .clear
         // imageView.image = image
-        imageBuffer.append(image!)
+        imageBuffer.append(image)
         locationBuffer.append(curLocation!)
         print("location count")
         print(locationBuffer.count)
@@ -160,9 +161,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         bufferCounter.text = String(imageBuffer.count)
         self.view.addSubview(imageView)
         print("photoOutput")
-        let rgba = RGBAImage(image: image!)!
-        print(rgba.pixels)
-        print(image?.pixelData())
+        print(image.pixelData())
     }
     
     
@@ -179,93 +178,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.captureSession.stopRunning()
     }
     
-}
-
-public struct Pixel {
-    
-    //각 색상 컴포넌트는 ABGR 순으로 저장되어 있다.
-    public var value: UInt32
-    
-    //red
-    public var R: UInt8 {
-        get { return UInt8(value & 0xFF); }
-        set { value = UInt32(newValue) | (value & 0xFFFFFF00) }
+    func imageWithImage(image:UIImage, scaledToSize newSize:CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+        image.draw(in: CGRect(origin: CGPoint.zero, size: CGSize(width: newSize.width, height: newSize.height)))
+        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
     }
     
-    //green
-    public var G: UInt8 {
-        get { return UInt8((value >> 8) & 0xFF) }
-        set { value = (UInt32(newValue) << 8) | (value & 0xFFFF00FF) }
-    }
-    
-    //blue
-    public var B: UInt8 {
-        get { return UInt8((value >> 16) & 0xFF) }
-        set { value = (UInt32(newValue) << 16) | (value & 0xFF00FFFF) }
-    }
-    
-    //alpha
-    public var A: UInt8 {
-        get { return UInt8((value >> 24) & 0xFF) }
-        set { value = (UInt32(newValue) << 24) | (value & 0x00FFFFFF) }
-    }
-}
-
-public struct RGBAImage {
-    public var pixels: UnsafeMutableBufferPointer<Pixel>
-    public var width: Int
-    public var height: Int
-    
-    public init?(image: UIImage) {
-        // CGImage로 변환이 가능해야 한다.
-        guard let cgImage = image.cgImage else {
-            return nil
-        }
-        
-        // 주소 계산을 위해서 Float을 Int로 저장한다.
-        width = Int(image.size.width)
-        height = Int(image.size.height)
-        
-        // 4 * width * height 크기의 버퍼를 생성한다.
-        let bytesPerRow = width * 4
-        let imageData = UnsafeMutablePointer<Pixel>.allocate(capacity: width * height)
-        
-        // 색상공간은 Device의 것을 따른다
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        
-        // BGRA로 비트맵을 만든다
-        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
-        bitmapInfo = bitmapInfo | CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
-        
-        // 비트맵 생성
-        guard let imageContext = CGContext(data: imageData, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo) else {
-            return nil
-        }
-        
-        // cgImage를 imageData에 채운다.
-        imageContext.draw(cgImage, in: CGRect(origin: .zero, size: image.size))
-        
-        pixels = UnsafeMutableBufferPointer<Pixel>(start: imageData, count: width * height)
-    }
-    
-    public func toUIImage() -> UIImage? {
-        let colorSpace = CGColorSpaceCreateDeviceRGB()
-        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Big.rawValue
-        let bytesPerRow = width * 4
-        
-        bitmapInfo |= CGImageAlphaInfo.premultipliedLast.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
-        
-        guard let imageContext = CGContext(data: pixels.baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo, releaseCallback: nil, releaseInfo: nil) else {
-            return nil
-        }
-        
-        guard let cgImage = imageContext.makeImage() else {
-            return nil
-        }
-        
-        let image = UIImage(cgImage: cgImage)
-        return image
-    }
 }
 
 extension UIImage {
